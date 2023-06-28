f7b5486e0dd082e10dc8b0b22a8e70488fba6b6f
Michael Mainer <8527305+MIchaelMainer@users.noreply.github.com>
2023-06-28 15:42:44 -0700

    Support standard linter output

--- branch_protection_export.ps1
+++ branch_protection_export.ps1
@@ -125,70 +125,70 @@ $sb = [System.Text.StringBuilder]::new($branch_protection_policy_file_contents)
 
 $nodes | ForEach-Object {
 
-    [void]$sb.AppendLine("`n  - branchNamePattern: $($_.pattern.ToString())")
+    [void]$sb.AppendLine("`n    - branchNamePattern: $($_.pattern.ToString())")
 
     if ($_.matchingRefs.nodes.count -gt 0) {
-        [void]$sb.AppendLine("    # This branch pattern applies to the following branches as of $(Get-Date):")
+        [void]$sb.AppendLine("      # This branch pattern applies to the following branches as of $(Get-Date):")
         $_.matchingRefs.nodes | ForEach-Object {
-            [void]$sb.AppendLine("    # $($_.name.ToString())")
+            [void]$sb.AppendLine("      # $($_.name.ToString())")
         }
     }
     else {
-        [void]$sb.AppendLine("    # This branch pattern does not apply to any currently existent branches as of $(Get-Date).")
+        [void]$sb.AppendLine("      # This branch pattern does not apply to any currently existent branches as of $(Get-Date).")
     }
 
-    [void]$sb.AppendLine("`n    # Specifies whether this branch can be deleted. boolean")
-    [void]$sb.AppendLine("    allowsDeletions: $($_.allowsDeletions.ToString().ToLower())")
+    [void]$sb.AppendLine("`n      # Specifies whether this branch can be deleted. boolean")
+    [void]$sb.AppendLine("      allowsDeletions: $($_.allowsDeletions.ToString().ToLower())")
 
-    [void]$sb.AppendLine("    # Specifies whether forced pushes are allowed on this branch. boolean")
-    [void]$sb.AppendLine("    allowsForcePushes: $($_.allowsForcePushes.ToString().ToLower())")
+    [void]$sb.AppendLine("      # Specifies whether forced pushes are allowed on this branch. boolean")
+    [void]$sb.AppendLine("      allowsForcePushes: $($_.allowsForcePushes.ToString().ToLower())")
 
-    [void]$sb.AppendLine("    # Specifies whether new commits pushed to the matching branches dismiss pull request review approvals. boolean")
-    [void]$sb.AppendLine("    dismissStaleReviews: $($_.dismissesStaleReviews.ToString().ToLower())")
+    [void]$sb.AppendLine("      # Specifies whether new commits pushed to the matching branches dismiss pull request review approvals. boolean")
+    [void]$sb.AppendLine("      dismissStaleReviews: $($_.dismissesStaleReviews.ToString().ToLower())")
 
-    [void]$sb.AppendLine("    # Specifies whether admins can overwrite branch protection. boolean")
-    [void]$sb.AppendLine("    isAdminEnforced: $($_.isAdminEnforced.ToString().ToLower())")
+    [void]$sb.AppendLine("      # Specifies whether admins can overwrite branch protection. boolean")
+    [void]$sb.AppendLine("      isAdminEnforced: $($_.isAdminEnforced.ToString().ToLower())")
 
     # https://stackoverflow.com/questions/76384359/github-graphql-api-branch-protection-rule-how-do-i-get-require-a-pull-reque
     if (($_.requiredApprovingReviewCount -ne $null) -and ($_.requiresApprovingReviews.ToString().ToLower() -eq "true")) {
-        [void]$sb.AppendLine("    # Indicates whether `"Require a pull request before merging`" is enabled. boolean")
-        [void]$sb.AppendLine("    requiresPullRequestBeforeMerging: $($_.requiresApprovingReviews.ToString().ToLower())")
+        [void]$sb.AppendLine("      # Indicates whether `"Require a pull request before merging`" is enabled. boolean")
+        [void]$sb.AppendLine("      requiresPullRequestBeforeMerging: $($_.requiresApprovingReviews.ToString().ToLower())")
     }
 
     if ($_.requiredApprovingReviewCount -ne $null) {
-        [void]$sb.AppendLine("    # Specifies the number of pull request reviews before merging. int (0-6). Should be null/empty if PRs are not required")
-        [void]$sb.AppendLine("    requiredApprovingReviewsCount: $($_.requiredApprovingReviewCount.ToString())")
+        [void]$sb.AppendLine("      # Specifies the number of pull request reviews before merging. int (0-6). Should be null/empty if PRs are not required")
+        [void]$sb.AppendLine("      requiredApprovingReviewsCount: $($_.requiredApprovingReviewCount.ToString())")
     }
 
-    [void]$sb.AppendLine("    # Require review from Code Owners. Requires requiredApprovingReviewsCount. boolean")
-    [void]$sb.AppendLine("    requireCodeOwnersReview: $($_.requiresCodeOwnerReviews.ToString().ToLower())")
+    [void]$sb.AppendLine("      # Require review from Code Owners. Requires requiredApprovingReviewsCount. boolean")
+    [void]$sb.AppendLine("      requireCodeOwnersReview: $($_.requiresCodeOwnerReviews.ToString().ToLower())")
 
-    [void]$sb.AppendLine("    # Are commits required to be signed. boolean. TODO: all contributors must have commit signing on local machines.")
-    [void]$sb.AppendLine("    requiresCommitSignatures: $($_.requiresCommitSignatures.ToString().ToLower())")
+    [void]$sb.AppendLine("      # Are commits required to be signed. boolean. TODO: all contributors must have commit signing on local machines.")
+    [void]$sb.AppendLine("      requiresCommitSignatures: $($_.requiresCommitSignatures.ToString().ToLower())")
 
-    [void]$sb.AppendLine("    # Are conversations required to be resolved before merging? boolean")
-    [void]$sb.AppendLine("    requiresConversationResolution: $($_.requiresConversationResolution.ToString().ToLower())")
+    [void]$sb.AppendLine("      # Are conversations required to be resolved before merging? boolean")
+    [void]$sb.AppendLine("      requiresConversationResolution: $($_.requiresConversationResolution.ToString().ToLower())")
 
-    [void]$sb.AppendLine("    # Are merge commits prohibited from being pushed to this branch. boolean")
-    [void]$sb.AppendLine("    requiresLinearHistory: $($_.requiresLinearHistory.ToString().ToLower())")
+    [void]$sb.AppendLine("      # Are merge commits prohibited from being pushed to this branch. boolean")
+    [void]$sb.AppendLine("      requiresLinearHistory: $($_.requiresLinearHistory.ToString().ToLower())")
 
     if ($_.requiresStatusChecks -eq $true) {
-        [void]$sb.AppendLine("    # Required status checks to pass before merging. Values can be any string, but if the value does not correspond to any existing status check, the status check will be stuck on pending for status since nothing exists to push an actual status")
-        [void]$sb.AppendLine("    requiredStatusChecks:")
+        [void]$sb.AppendLine("      # Required status checks to pass before merging. Values can be any string, but if the value does not correspond to any existing status check, the status check will be stuck on pending for status since nothing exists to push an actual status")
+        [void]$sb.AppendLine("      requiredStatusChecks:")
         $_.requiredStatusCheckContexts | ForEach-Object {
             [void]$sb.AppendLine("    - $($_.ToString())")
         }
     }
 
-    [void]$sb.AppendLine("    # Require branches to be up to date before merging. Requires requiredStatusChecks. boolean")
-    [void]$sb.AppendLine("    requiresStrictStatusChecks: $($_.requiresStrictStatusChecks.ToString().ToLower())")
+    [void]$sb.AppendLine("      # Require branches to be up to date before merging. Requires requiredStatusChecks. boolean")
+    [void]$sb.AppendLine("      requiresStrictStatusChecks: $($_.requiresStrictStatusChecks.ToString().ToLower())")
 
-    [void]$sb.AppendLine("    # Indicates whether there are restrictions on who can push. boolean. Should be set with whoCanPush.")
-    [void]$sb.AppendLine("    restrictsPushes: $($_.restrictsPushes.ToString().ToLower())")
+    [void]$sb.AppendLine("      # Indicates whether there are restrictions on who can push. boolean. Should be set with whoCanPush.")
+    [void]$sb.AppendLine("      restrictsPushes: $($_.restrictsPushes.ToString().ToLower())")
 
     if (($_.restrictsPushes.ToString().ToLower() -eq "true") -and ($_.pushAllowances.nodes.Count -gt 0)) {
-        [void]$sb.AppendLine("    # List of Apps, Users, and Teams that can push to this branch.")
-        [void]$sb.AppendLine("    whoCanPush:")
+        [void]$sb.AppendLine("      # List of Apps, Users, and Teams that can push to this branch.")
+        [void]$sb.AppendLine("      whoCanPush:")
         $_.pushAllowances.nodes | ForEach-Object {
             if ($_.actor.login -ne $null) {
                 [void]$sb.AppendLine("    - $($_.actor.login.ToString())")
@@ -199,12 +199,12 @@ $nodes | ForEach-Object {
         }
     }
 
-    [void]$sb.AppendLine("    # Restrict who can dismiss pull request reviews. boolean")
+    [void]$sb.AppendLine("      # Restrict who can dismiss pull request reviews. boolean")
     [void]$sb.AppendLine("    restrictsReviewDismissals: $($_.restrictsReviewDismissals.ToString().ToLower())")
 
     if (($_.restrictsReviewDismissals.ToString().ToLower() -eq "true") -and ($_.reviewDismissalAllowances.nodes.Count -gt 0)) {
-        [void]$sb.AppendLine("    # List of Apps, Users, and Teams that can dismiss pull request reviews to this branch pattern.")
-        [void]$sb.AppendLine("    whoCanDismissReviews:")
+        [void]$sb.AppendLine("      # List of Apps, Users, and Teams that can dismiss pull request reviews to this branch pattern.")
+        [void]$sb.AppendLine("      whoCanDismissReviews:")
         $_.reviewDismissalAllowances.nodes | ForEach-Object {
             if ($_.actor.login -ne $null) {
                 [void]$sb.AppendLine("    - $($_.actor.login.ToString())")
